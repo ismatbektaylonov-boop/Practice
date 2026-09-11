8bf0b1dda234b8d00f1ee61bb40c6190e6032823
ismatbektaylonov <ismatbektaylonov@gmail.com>
2026-09-11 17:54:22 +0900

    fix: learn scope and priority

--- functions.py
+++ functions.py
@@ -36,7 +36,7 @@ def give_greet(name, age=22):
     print("give_greet is executed")
     return f"Hi {name}, you are {age} years old!"
 
-
+ 
 # CALL
 result3 = give_greet(name="Justin", age=28)
 print("result3:", result3)
@@ -45,3 +45,13 @@ result4 = give_greet("John")
 print("result4:", result4)
 
 print("=========Scope=========")
+b = 100  # 3
+
+# DEFINE
+def calculate(a):  # 2
+    c = a * b  # 1
+    print(f"the c value: {c}")
+
+
+# CALL
+calculate(5)
