5367a6502f5546f9e83fb2d1d5421fe97739eca8
ismatbektaylonov <ismatbektaylonov@gmail.com>
2026-09-11 23:47:02 +0900

    E-TASK: completed

--- train.js
+++ train.js
@@ -1,13 +1,35 @@
+// TASK F:
+
+// Yagona string argumentga ega findDoublers nomli function tuzing
+// Agar stringda bittadan ortiq bir xil harflar ishtirok etgan bo'lsa
+// true yokida false natija qaytarsin.
+
+// MASALAN: findDoublers("hello"); natija true qaytadi. Sababi ikki marotaba takrorlangan 'll' harfi mavjud!
+
+function findDoublers(str) {
+	for (let i = 0; i < str.length; i++) {
+		for (let j = i + 1; j < str.length; j++) {
+			if (str[i] === str[j]) {
+				return true
+			}
+		}
+	}
+	return false
+}
+
+const result = findDoublers('hello')
+console.log(result)
+
 // E-TASK:
 
 // Shunday function tuzing, u bitta string argumentni qabul qilib osha stringni teskari qilib return qilsin.
 // MASALAN: getReverse("hello") return qilsin "olleh"
 
-function getReverse(a) {
-	const b = a.split('').reverse().join('')
-	// console.log(b)
-	return b
-}
+// function getReverse(a) {
+// 	const b = a.split('').reverse().join('')
+// 	// console.log(b)
+// 	return b
+// }
 
-const result = getReverse('hello')
-console.log(result)
+// const result = getReverse('hello')
+// console.log(result)
