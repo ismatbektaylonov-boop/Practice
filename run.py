109d1c65d9f1b369f2552a3881e8222f498b1fab
ismatbektaylonov <ismatbektaylonov@gmail.com>
2026-09-11 17:09:29 +0900

    feat: Python system variables & special methods via Dunders

--- run.py
+++ run.py
@@ -1,4 +1,15 @@
-a = 100
-print("a:", a)
-message = "Hello, World!"
-print("message:", message)
+# Dunder methods __builtins__, __init__
+message = "Python: Everything is an object!"
+print(message)
+
+result = type(message)
+print("result: ", result)
+
+'''
+In Python, there are built-in tools:
+(1)TYPES -> int, float, str, list, dict, tuple, set, bool
+(2)FUNCTIONS -> print(), len(), type(), input(), range(), sum(), min(), max(), sorted()
+(3)EXCEPTIONS -> True, False, None, ValueError, TypeError, IndexError, KeyError, ZeroDivisionError
+'''
+
+print(dir(__builtins__))  # List all built-in functions and types
