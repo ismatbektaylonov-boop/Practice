a63c2d639477947611f65974401d4e10a4713e49
ismatbektaylonov <ismatbektaylonov@gmail.com>
2026-09-14 21:21:17 +0900

    practice: review

--- practice.py
+++ practice.py
@@ -19,7 +19,7 @@ class Shop():
         print(f"{self.non}ta non, {self.cola}ta cola, {self.muzqaymoq}ta muzqaymoq")
 
     def sotish(self, name, amount):
-        if (name == 'non'):
+        if (name == 'non' ):
             if (self.non > amount):
                 self.non -= amount
                 print(f"{name} {self.non}ta qoldi")
@@ -61,5 +61,118 @@ products = Shop(3, 6, 7)
 products.qoldiq()
 # products.sotish('muzqaymoq', 2)
 products.qabul('cola', 2)
+products.sotish('pepsi', 2)
 # products.qabul('cola', 3)
-# products.qoldiq()
+products.qoldiq()
+
+# python in use:
+'''
+1. compiled language
+php, java, c++, c#
+2. interpreted language
+python, js, nodejs
+
+git config --global user.name "Your Name"
+git config --global user.email "your_email@example.com"
+
+In Python, there are built-in tools:
+(1)TYPES -> int, float, str, list, dict, tuple, set, bool
+(2)FUNCTIONS -> print(), len(), type(), input(), range(), sum(), min(), max(), sorted()
+(3)EXCEPTIONS -> True, False, None
+
+__dunder methods__ (__builtins__, __init__)
+'''
+# print(dir(__builtins__))
+'''
+
+primitive variable types:
+1. string (type, tittle, upper, replace)
+'''
+# message = "Python: everything is an object!"
+# print(message)
+
+# result = type(message)
+# print("result: ", result)
+
+# result = message.title()
+# print("result: ", result)
+
+# result = message.upper()
+# print("result: ", result)
+
+# result = message.replace("object", "class")
+# print("result: ", result)
+# print(message)
+'''
+2. numbers (int, float)
+methods and states:
+'''
+# import math
+# from math import ceil, asin
+# import array # package/module
+# count = 100
+# count_type = type(count)
+# print("count: ", count, "count_type: ", count_type)
+# print(f"count: {count}, count_type: {count_type}")
+
+# result1 = count.bit_count() # methods
+# print(f"result1: {result1}")
+
+# result2 = count.from_number(1) # state
+# print(f"result2: {result2}")
+
+# result1 = ceil(97.7)  # CALL
+# print("result1:", result1)
+
+# print("=========array=========")
+# print(type(array))
+# print(type(math))
+# print(type(ceil))
+# print(type(asin))
+
+'''
+3. boolean (type, input, bool)
+(isnumeric, )
+truthy -> True, 1, -1, "1", "True", " ", [], {} ()
+and falsy -> False, 0, "", None
+
+'''
+
+# y = input("Enter your name: ")
+# print(y)
+# print(type(y))
+# print(y.isnumeric())
+# print(bool(y))
+'''
+
+functions:
+1. define vs call (indentation)
+2. parameters vs arguments
+3. keyword vs default arguments
+'''
+def greet(name, age):
+    return f"How do you do {name} , you are {age} years old!"
+    # return True
+
+# call
+result = greet("Martin", 33)
+print(result)
+'''
+4. scope
+'''
+b=7 # 3
+def calculator(a, b=5): # 2
+    c = a + b
+    print(f"the c value: {c}")
+    # return c
+
+# call  
+calculator(2, 3) # 1
+'''
+objects:
+1. what is object (array, math(ceil))
+2. iterable objects (for in) & RANGE (range())
+(string, dict, tuple, list, range, map, filter)
+3. DICTIONARY (json object)
+4. Error handling system
+'''
